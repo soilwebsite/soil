d96d548be3632e56b29e05cdfe9aff1cba3e88f4
Jake Nelken <jnelks@gmail.com>
2017-01-29 15:32:51 -0500

    Add more depth to ImageGrid.

--- src/components/ImageGrid/index.js
+++ src/components/ImageGrid/index.js
@@ -4,12 +4,35 @@ class Navbar extends Component {
 
   images () {
     const {images} = this.props;
-    return images.map(image => {
-      return (
-        <div className='image'>
-          <img src={image.url} alt={image.name} />
-        </div>
-      )
+    const manyImages = images.concat(images);
+    return manyImages.map((image, i) => {
+      if (i % 4 === 0) {
+        return (
+          <div className='big grid-item'>
+            <img src={image.url} alt={image.name} />
+          </div>
+        )
+      }
+      else if (!manyImages[i+1]) {
+        return (
+          <div className='grid-item'>
+            <img src={image.url} alt={image.name} />
+          </div>
+        )
+      }
+      else if (i % 5 === 1 || i % 5 === 3) {
+        return (
+          <div className='two'>
+            <div className='grid-item'>
+              <img src={image.url} alt={image.name} />
+            </div>
+            <div className='grid-item'>
+              <img src={manyImages[i+1].url} alt={manyImages[i+1].name} />
+            </div>
+          </div>
+        )
+      }
+      return null;
     })
   }
   render() {
