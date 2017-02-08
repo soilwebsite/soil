3414a0218d14a37a3a3a1028751963e8b95329c1
Jake Nelken <jnelks@gmail.com>
2017-02-08 14:37:40 -0500

    Add homepage buttons.

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -12,18 +12,31 @@ class Homepage extends Component {
   }
 
   render() {
-    let videoURL = 'http://localhost:3000/forest.mp4';
-    // <div className="hero" />
     return (
       <div className="Homepage">
-        <video
-          ref='video'
-          autoPlay
-          style={{objectFit:'cover', width:'100%', height:'100%'}}
-          src={videoURL}
-        />
+        <h2 class="textTitle">The Campaign</h2>
+        <p>Discover the Summer 2017 campaign.</p>
+        <div className="buttons">
+          <a href="#">
+            PRE-ORDER THE COLLECTION
+          </a>
+          &nbsp;
+          <a href="#">
+            EXPLORE THE CAMPAIGN
+          </a>
+        </div>
+        <div className='video-container'>
+          <video
+            ref='video'
+            autoPlay
+            muted
+            style={{objectFit:'cover', width:'100%', height:'100%'}}
+            src={'http://localhost:3000/forest.mp4'}
+            />
+        </div>
       </div>
     );
+    // <div className="hero" />
   }
 }
 
