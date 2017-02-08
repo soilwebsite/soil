1202914eaa6c46cba4074200f0a952471286ac54
Jake Nelken <jnelks@gmail.com>
2017-02-08 01:38:28 -0500

    Add production start script (still does not run minified code perfectly yet).

--- scripts/prod.js
+++ scripts/prod.js
@@ -1,13 +1,21 @@
+/// added
+require('../db');
+process.env.NODE_ENV = 'production';
+///
+
 const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('port', 3031);
+
+const port = process.env.PORT || 3031;
+app.set('port', port);
 
 if(process.env.NODE_ENV === 'production') {
   // Serve static assets
   // app.use(express.static('build'));
   app.use(express.static(path.resolve(__dirname, '..', 'build')));
+  console.log('√ Production build');
 }
 
 // Always return the main index.html, so react-router render the route in the client
@@ -15,5 +23,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
+
+/// Start server
+
+app.listen(app.get('port'), () => {
+  console.log(`Server started at: http://localhost:${app.get('port')}/`);
+})
 
 module.exports = app;
