eacbb0ba5e20701c2e6f8b2cb2752d18343b2418
Jake Nelken <jnelks@gmail.com>
2017-02-26 02:17:27 -0500

    Add Credentials component.

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Credentials from '../components/Credentials'
 
 class SignIn extends Component {
   render() {
     return (
       <div className="SignIn">
-        SignIn
+        <Credentials />
       </div>
     );
   }
