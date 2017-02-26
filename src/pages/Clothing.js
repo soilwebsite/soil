d4d29b92c7fd75840529136284b714c2b40cacc1
Jake Nelken <jnelks@gmail.com>
2017-02-26 00:56:17 -0500

    Remove nested component folders.

--- src/pages/Clothing.js
+++ src/pages/Clothing.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Sidebar from '../../components/Sidebar';
-import ProductGrid from '../../components/ProductGrid';
+import Sidebar from '../components/Sidebar';
+import ProductGrid from '../components/ProductGrid';
 
 const sidebarItems = [
   { name: 'Tops' },
