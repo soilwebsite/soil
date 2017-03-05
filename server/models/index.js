c2112fb5aa4ff90682bf3275d68233bf3fcfedb3
Jake Nelken <jnelks@gmail.com>
2017-03-05 03:16:57 -0500

    Add tags and product_tag associations. Seed associations and tags.

--- server/models/index.js
+++ server/models/index.js
@@ -5,20 +5,69 @@ if (!global.hasOwnProperty('db')) { // check to see if sequelize is defined yet
   let sequelize;
 
   const seedDB = () => {
-    return global.db.Product.bulkCreate([{
-      name: 'Solid Black Top',
-      imageUrl: 'http://wpexplorer-demos.com/photopro/wp-content/uploads/sites/12/2012/10/Sasha.jpg'
+      return global.db.Tag.bulkCreate([{
+        name: 'tops'
       }, {
-      name: 'Studded Leather Vest',
-      imageUrl: 'https://s-media-cache-ak0.pinimg.com/736x/47/fc/c2/47fcc2871992396a5a085e155b92a537.jpg'
+        name: 'bottoms'
       }, {
-      name: 'Nice Pearl Sweater',
-      imageUrl: 'http://wpexplorer-demos.com/photopro/wp-content/uploads/sites/12/2012/10/Kaitlyn.jpg'
+        name: 'dresses',
       }, {
-      name: 'Tribal Headwrap',
-      imageUrl: 'http://wpexplorer-demos.com/photopro/wp-content/uploads/sites/12/2012/10/Gloria.jpg'
+        name: 'outerwear'
+      }, {
+        name: 'accessories'
       }
-    ]);
+    ])
+    .then(() => {
+      return global.db.Product.bulkCreate([{
+        name: 'Solid Black Top',
+        imageUrl: 'http://wpexplorer-demos.com/photopro/wp-content/uploads/sites/12/2012/10/Sasha.jpg'
+        }, {
+        name: 'Studded Leather Bikini',
+        imageUrl: 'https://s-media-cache-ak0.pinimg.com/736x/47/fc/c2/47fcc2871992396a5a085e155b92a537.jpg'
+        }, {
+        name: 'Nice Pearl Dress',
+        imageUrl: 'http://wpexplorer-demos.com/photopro/wp-content/uploads/sites/12/2012/10/Kaitlyn.jpg'
+        }, {
+        name: 'Tribal Headwrap',
+        imageUrl: 'http://wpexplorer-demos.com/photopro/wp-content/uploads/sites/12/2012/10/Gloria.jpg'
+        }
+      ])
+    })
+    .then(products => {
+      return global.db.Tag.findById(1).then((tag) => products[0].addTag())
+        .then(global.db.Tag.findById(2).then((tag) => products[1].addTag()))
+        .then(global.db.Tag.findById(3).then((tag) => products[3].addTag()))
+        .then(global.db.Tag.findById(4).then((tag) => products[3].addTag()))
+    })
+    .then(() => {
+      global.db.Tag.findById(1).then(tag => {
+        global.db.Product.findById(1).then(product => {
+          product.addTag(tag)
+        })
+      })
+      global.db.Tag.findById(2).then(tag => {
+        global.db.Product.findById(2).then(product => {
+          product.addTag(tag)
+        })
+      })
+      global.db.Tag.findById(3).then(tag => {
+        global.db.Product.findById(3).then(product => {
+          product.addTag(tag)
+        })
+      })
+      global.db.Tag.findById(4).then(tag => {
+        global.db.Product.findById(4).then(product => {
+          product.addTag(tag)
+        })
+      })
+      global.db.Tag.findById(5).then(tag => {
+        global.db.Product.findById(4).then(product => {
+          product.addTag(tag)
+        })
+      })
+        // .then(() => global.db.Product.findOne({ where: { id: 1 }, include: global.db.Tag }))
+        // .then(res => console.log(res.dataValues.tags))
+    })
   }
 
   if (process.env.NODE_ENV === 'production') {
@@ -45,7 +94,8 @@ if (!global.hasOwnProperty('db')) { // check to see if sequelize is defined yet
     Sequelize: Sequelize,
     sequelize: sequelize,
     User:      sequelize.import(__dirname + '/user'),
-    Product:   sequelize.import(__dirname + '/product')
+    Product:   sequelize.import(__dirname + '/product'),
+    Tag:       sequelize.import(__dirname + '/tag')
     // add your other models here
   }
 
@@ -53,6 +103,8 @@ if (!global.hasOwnProperty('db')) { // check to see if sequelize is defined yet
     Associations can be defined here. E.g. like this:
     global.db.User.hasMany(global.db.SomethingElse)
   */
+  global.db.Product.belongsToMany(global.db.Tag, {through: 'products_tags'});
+  global.db.Tag.belongsToMany(global.db.Product, {through: 'products_tags'})
 
   sequelize
     .authenticate()
