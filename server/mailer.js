8da35cca754303dc2ef13f67a7711c88ad2db3d2
Jake Nelken <jnelks@gmail.com>
2017-06-23 00:25:47 -0400

    Add nodemailer and turn off mail function.

--- server/mailer.js
+++ server/mailer.js
@@ -1,27 +1,26 @@
 const nodemailer = require('nodemailer');
 
-// create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
     service: 'zoho',
     auth: {
-        user: 'maneesha@baikhal.com',
-        pass: process.env.REACT_APP_EMAIL_PW
+      user: 'maneesha@baikhal.com',
+      pass: process.env.REACT_APP_EMAIL_PW
     }
 });
 
-// setup email data with unicode symbols
 let mailOptions = {
     from: '"Maneesha W" <maneesha@baikhal.com>', // sender address
-    to: 'jnelks@gmail.com', // list of receivers
+    to: 'maneesha.wijesinghe1@gmail.com', // list of receivers
     subject: 'Hello ✔', // Subject line
-    text: 'Hello world ?', // plain text body
-    html: '<b>Hello world ?</b>' // html body
+    text: 'I love you', // plain text body
+    html: '<b>I love you</b>' // html body
 };
 
-// send mail with defined transport object
+// un-comment this to send an email:
+
 // transporter.sendMail(mailOptions, (error, info) => {
 //     if (error) {
-//         return console.log(error);
+//       return console.log(error);
 //     }
 //     console.log('Message %s sent: %s', info.messageId, info.response);
 // });
