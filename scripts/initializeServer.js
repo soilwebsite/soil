4887fe737b6a70846f7e58459cdbb3e0213427bc
Jake Nelken <jnelks@gmail.com>
2017-09-28 19:12:39 -0400

    HTTPS

--- scripts/initializeServer.js
+++ scripts/initializeServer.js
@@ -4,6 +4,16 @@ module.exports = (app) => {
   app.use(bodyParser.json({ strict: true }))
   app.use(bodyParser.urlencoded({ extended: true }))
 
+  /*
+    Redirect http to https using Heroku x-header
+    https://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect
+   */
+  app.get('*', function(req,res,next) {
+    if(req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production')
+      res.redirect('https://' + req.hostname + req.url)
+    else
+      next() /* Continue to other routes if we're not redirecting */
+  })
   app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', process.env.REACT_APP_DOMAIN)
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import shopifyWidget from './shopifyWidget'
+// import shopifyWidget from './shopifyWidget'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -16,18 +16,18 @@ class ProductDetail extends Component {
   render() {
     const { item } = this.props
 
-    return (
-      <div id='product-component-1506116392081'>
-        {shopifyWidget(item.product_id)}
-      </div>
-    )
     // return (
-    //   <Container>
-    //     <img src={item.images[0].src} alt={item.title} />
-    //     {item.title}
-    //     {item.tags ? item.tags.split(' ').map(tag => <span key={tag}>#{tag}</span>) : null}
-    //   </Container>
+    //   <div id='product-component-1506116392081'>
+    //     {shopifyWidget(item.product_id)}
+    //   </div>
     // )
+    return (
+      <Container>
+        <img src={item.images[0].src} alt={item.title} />
+        {item.title}
+        {item.tags ? item.tags.split(' ').map(tag => <span key={tag}>#{tag}</span>) : null}
+      </Container>
+    )
   }
 }
 
