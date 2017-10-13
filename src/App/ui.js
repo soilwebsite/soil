840a4f4c35e0ad63c08d5aa81c8f0da0627128a2
Jake Nelken <jnelks@gmail.com>
2017-10-12 22:46:09 -0400

    Update font AGAIN

--- src/App/ui.js
+++ src/App/ui.js
@@ -4,13 +4,14 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   color: ${({ theme }) => theme.color.text};
+  font-family: 'Raleway', sans-serif;
   h1,
   h2,
   h3,
   h4,
   h5,
   h6 {
-    font-family: 'franklingothic-demi', sans-serif;
+    font-weight: bold;
   }
   h2 {
     font-size: 2em;
