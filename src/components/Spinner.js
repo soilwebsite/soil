0c1da2701e9b1556d10b371f1a56f0b434a4b790
Jake Nelken <jnelks@gmail.com>
2017-12-29 13:46:02 -0500

    Simplify spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,15 +1,9 @@
-import React from 'react'
 import styled from 'styled-components'
 
-const SpinnerUI = styled.div`
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-
-  .loading,
-  .loading::before,
-  .loading::after {
+export default styled.div`
+  &,
+  &::before,
+  &::after {
     position: absolute;
     top: 50%;
     left: 50%;
@@ -17,45 +11,28 @@ const SpinnerUI = styled.div`
     border-left-color: rgba(0, 0, 0, 0.4);
     border-radius: 999px;
   }
-
-  .loading {
-    margin: -25px 0 0 -25px;
-    height: 50px;
-    width: 50px;
-    animation: animation-rotate 1000ms linear infinite;
-  }
-
-  .loading::before {
+  margin: -25px 0 0 -25px;
+  margin-top: -80px;
+  height: 50px;
+  width: 50px;
+  animation: animation-rotate 1000ms linear infinite;
+  &::before {
     content: '';
     margin: -23px 0 0 -23px;
     height: 44px;
     width: 44px;
     animation: animation-rotate 1000ms linear infinite;
   }
-
-  .loading::after {
+  &::after {
     content: '';
     margin: -29px 0 0 -29px;
     height: 56px;
     width: 56px;
     animation: animation-rotate 2000ms linear infinite;
   }
-
   @keyframes animation-rotate {
     100% {
       transform: rotate(360deg);
     }
   }
 `
-
-class Spinner extends React.Component {
-  render() {
-    return (
-      <SpinnerUI>
-        <div className="loading" />
-      </SpinnerUI>
-    )
-  }
-}
-
-export default Spinner
